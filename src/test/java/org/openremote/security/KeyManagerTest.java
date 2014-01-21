a2463d0a22478d301da6250e31d45a8f0822f7b6
Juha Lindfors <juha@openremote.org>
2014-01-21 02:40:05 +0100

    adds some more tests

--- src/test/java/org/openremote/security/KeyManagerTest.java
+++ src/test/java/org/openremote/security/KeyManagerTest.java
@@ -20,11 +20,20 @@
  */
 package org.openremote.security;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.security.KeyStore;
+import java.security.Provider;
+import java.security.Security;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -171,6 +180,221 @@ public class KeyManagerTest
     }
   }
 
+
+  /**
+   * Test behavior when loading keystore with wrong password.
+   *
+   * @throws Exception  if test fails
+   */
+  @Test public void testWrongPassword() throws Exception
+  {
+    JCEKSStorage ks = new JCEKSStorage();
+
+    File dir = new File(System.getProperty("user.dir"));
+    File f = new File(dir, "test.keystore." + UUID.randomUUID());
+    f.deleteOnExit();
+
+    ks.add(
+        "alias",
+        new KeyStore.SecretKeyEntry(
+            new SecretKeySpec(new byte[] { 'a' }, "test")
+        ),
+        new KeyStore.PasswordProtection(new char[] { 'b' })
+    );
+
+    char[] password = new char[] { 'f', 'o', 'o' };
+
+    ks.save(f, password);
+
+    try
+    {
+      ks.save(f, new char[] { 0 });
+
+      Assert.fail("should not get here...");
+    }
+
+    catch (KeyManager.KeyManagerException e)
+    {
+      // expected...
+    }
+  }
+
+  /**
+   * Test behavior when a secret key is added to storage that does not
+   * support them.
+   *
+   * @throws Exception  if test fails
+   */
+  @Test public void testAddingSecretKeyToPKCS12() throws Exception
+  {
+    PKCS12Storage ks = new PKCS12Storage();
+
+    ks.add(
+        "alias",
+        new KeyStore.SecretKeyEntry(
+            new SecretKeySpec(new byte[] { 'a' }, "test")
+        ),
+        new KeyStore.PasswordProtection(new char[] { 'b' })
+    );
+
+    char[] password = new char[] { 'f', 'o', 'o' };
+
+    try
+    {
+      ks.save(password);
+
+      Assert.fail("should not get here...");
+    }
+
+    catch (KeyManager.KeyManagerException e)
+    {
+      // expected...
+    }
+  }
+
+  /**
+   * Tests adding a secret key to JCEKS storage.
+   *
+   * @throws Exception  if test fails
+   */
+  @Test public void testAddingSecretKeyToJCEKS() throws Exception
+  {
+    JCEKSStorage ks = new JCEKSStorage();
+
+    ks.add(
+        "alias",
+        new KeyStore.SecretKeyEntry(
+            new SecretKeySpec(new byte[] { 'a' }, "test")
+        ),
+        new KeyStore.PasswordProtection(new char[] { 'b' })
+    );
+
+    char[] password = new char[] { 'f', 'o', 'o' };
+
+    ks.save(password);
+  }
+
+  /**
+   * Tests adding a secret key to BKS storage.
+   *
+   * @throws Exception  if test fails
+   */
+  @Test public void testAddingSecretKeyToBKS() throws Exception
+  {
+    // BKS implementation requires "BC" to be available as security provider...
+    try
+    {
+      Security.addProvider(new BouncyCastleProvider());
+
+      BKSStorage ks = new BKSStorage();
+
+      ks.add(
+          "alias",
+          new KeyStore.SecretKeyEntry(
+              new SecretKeySpec(new byte[] { 'a' }, "test")
+          ),
+          new KeyStore.PasswordProtection(new char[] { 'b' })
+      );
+
+      char[] password = new char[] { 'f', 'o', 'o' };
+
+      ks.save(password);
+    }
+
+    finally
+    {
+      Security.removeProvider("BC");
+    }
+  }
+
+
+  /**
+   * Test implementation behavior when requested keystore algorithm is not available.
+   *
+   * @throws Exception  if test fails
+   */
+  @Test public void testAddingSecretKeyToUnavailableBKS() throws Exception
+  {
+    UnavailableBKS ks = new UnavailableBKS();
+
+    ks.add(
+        "alias",
+        new KeyStore.SecretKeyEntry(
+            new SecretKeySpec(new byte[] { 'a' }, "test")
+        ),
+        new KeyStore.PasswordProtection(new char[] { 'b' })
+    );
+
+    char[] password = new char[] { 'f', 'o', 'o' };
+
+    try
+    {
+      ks.save(password);
+
+      Assert.fail("Should not get here...");
+    }
+
+    catch (KeyManager.ConfigurationException e)
+    {
+      // expected...
+    }
+  }
+
+
+  /**
+   * Tests behavior when an existing keystore has been corrupted.
+   *
+   * @throws Exception  if test fails
+   */
+  @Test public void testCorruptJCEKS() throws Exception
+  {
+    JCEKSStorage ks = new JCEKSStorage();
+
+    ks.add(
+        "alias",
+        new KeyStore.SecretKeyEntry(
+            new SecretKeySpec(new byte[] { 'a' }, "test")
+        ),
+        new KeyStore.PasswordProtection(new char[] { 'b' })
+    );
+
+    char[] password = new char[] { 'f', 'o', 'o' };
+    File dir = new File(System.getProperty("user.dir"));
+    File f = new File(dir, "test.keystore." + UUID.randomUUID());
+    f.deleteOnExit();
+
+    ks.add(
+        "foobar",
+        new KeyStore.SecretKeyEntry(
+            new SecretKeySpec(new byte[] { 'a' }, "test")
+        ),
+        new KeyStore.PasswordProtection(new char[] { 'b' })
+    );
+
+    ks.save(f, password);
+
+    FileOutputStream fout = new FileOutputStream(f);
+    BufferedOutputStream bout = new BufferedOutputStream(fout);
+
+    bout.write("Add some garbage".getBytes());
+    bout.close();
+
+    password = new char[] { 'f', 'o', 'o' };
+
+    try
+    {
+      ks.save(f, password);
+
+      Assert.fail("should not get here...");
+    }
+
+    catch (KeyManager.KeyManagerException e)
+    {
+      // expected...
+    }
+  }
+
+
   /**
    * Tests storing a keystore with empty master password.
    *
@@ -205,6 +429,81 @@ public class KeyManagerTest
     // no op, just to test abstract superclass implementation...
   }
 
+  /**
+   * KeyManager for Sun JCEKS keystorage. This implementation searches for a correct provider
+   * that implements the Sun proprietary JCEKS storage, or asserts a failure of no security
+   * provider is found to execute the test.
+   */
+  private static class JCEKSStorage extends KeyManager
+  {
+    private static Provider findJCEKSProvider()
+    {
+      Map<String, String> props = new HashMap<String, String>();
+      props.put("keystore.jceks", "");
+      Provider[] providers = Security.getProviders(props);
 
+      if (providers.length == 0)
+      {
+        Assert.fail("Cannot find a security provider for Sun JCEKS");
+
+        return null;
+      }
+
+      else
+      {
+        return providers[0];
+      }
+    }
+
+    JCEKSStorage()
+    {
+      super(StorageType.JCEKS, findJCEKSProvider());
+    }
+  }
+
+  /**
+   * BKS keystorage from BouncyCastle.
+   */
+  private static class BKSStorage extends KeyManager
+  {
+    BKSStorage()
+    {
+      super(StorageType.BKS, new BouncyCastleProvider());
+    }
+  }
+
+  /**
+   * Test KeyManager implementation that attempts to load/create a keystore of a type
+   * that is not available in the security provider.
+   */
+  private static class UnavailableBKS extends KeyManager
+  {
+    UnavailableBKS()
+    {
+      super(StorageType.BKS, new EmptyProvider());
+    }
+  }
+
+  /**
+   * PKCS12 storage from a default security prover.
+   */
+  private static class PKCS12Storage extends KeyManager
+  {
+    PKCS12Storage()
+    {
+      super(StorageType.PKCS12, null);
+    }
+  }
+
+  /**
+   * An empty test security provider used for some test cases.
+   */
+  private static class EmptyProvider extends Provider
+  {
+    EmptyProvider()
+    {
+      super("Empty Test Provider", 0.0, "Testing");
+    }
+  }
 }
 
